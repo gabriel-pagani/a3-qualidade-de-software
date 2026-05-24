2b9b02addaa2415cbe7c7a92801336e8b6bf7c09
ffcand <167198548+ffcand@users.noreply.github.com>
2026-05-24 11:40:18 -0300

    feat: adiciona mais testes para validar os métodos da classe PasswordType
    feat: adiciona mais testes para validar os métodos da classe PasswordType

--- tests/test_controller_password_type.py
+++ tests/test_controller_password_type.py
@@ -57,3 +57,59 @@ def test_get_all_password_types():
 
     for pwt in password_types:
         assert pwt.name in ["Test", "Test2", "Test3"]
+
+
+@patch('controllers.password_type.execute_query')
+def test_get_all_password_types_exception(mock_execute_query):
+    mock_execute_query.side_effect = Exception("Erro simulado de banco de dados")
+    
+    password_types = PasswordType.get_all()
+    
+    assert password_types == []
+
+
+def test_update_password_type():
+    new_password_type = PasswordType.create(name="Test")
+    result = new_password_type.update(name="Test2")
+    password_type = PasswordType.get(id=new_password_type.id)
+
+    assert result is not False
+    assert password_type.name == "Test2"
+
+
+def test_update_password_type_invalid():
+    new_password_type = PasswordType.create(name="Test")
+    result = new_password_type.update(name=123)
+    password_type = PasswordType.get(id=new_password_type.id)
+
+    assert result is not True
+    assert password_type.name == "Test"
+
+
+@patch('controllers.password_type.execute_query')
+def test_update_password_type_exception(mock_execute_query):
+    mock_execute_query.side_effect = Exception("Erro simulado de banco de dados")
+    
+    new_password_type = PasswordType(id=1, name="Test")
+    result = new_password_type.update(name="Test2")
+    
+    assert result is False
+
+
+def test_delete_password_type():
+    new_password_type = PasswordType.create(name="Test")
+    result = new_password_type.delete()
+    password_type = PasswordType.get(id=new_password_type.id)
+
+    assert result is not False
+    assert password_type is None
+
+
+@patch('controllers.password_type.execute_query')
+def test_delete_password_type_exception(mock_execute_query):
+    mock_execute_query.side_effect = Exception("Erro simulado de banco de dados")
+    
+    new_password_type = PasswordType(id=1, name="Test")
+    result = new_password_type.delete()
+    
+    assert result is False
